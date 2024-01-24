1277d8729aae341fb7f40b3b68a8457e1ff566af
Lee Wilson <awilson21@luc.edu>
2024-01-24 16:41:24 +0000

    Finished SimpleCalc.c

--- bootcamp/WarmUp_2/SimpleCalc.c
+++ bootcamp/WarmUp_2/SimpleCalc.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char * * argv)
 {
 	int x;
-	scanf("Enter first number: %d", &x);
-	printf("Your number was: %d", x);
-	return 0;
+	int y;
+
+	printf("Enter the first number: ");
+	scanf("%d", &x);
+	printf("Enter the second number: ");
+	scanf("%d", &y);
+
+	printf("Sum: %d\n", (x + y));
+	printf("Difference: %d\n", (x - y));
+	printf("Product: %d\n", (x * y));
+	printf("Quotient: %d\n", (x / y));
+
+	return EXIT_SUCCESS;
 }

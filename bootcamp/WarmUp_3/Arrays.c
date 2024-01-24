9d644d0225e00f33aeccac5f9a6727cd34dd8dc8
Lee Wilson <awilson21@luc.edu>
2024-01-24 17:02:00 +0000

    Drafted arrays.c

---
+++ bootcamp/WarmUp_3/Arrays.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * * argv)
+{  
+    int size = 5;
+    int sum;
+    double average;
+
+    int arr[] = {1, 2, 3, 4, 5};
+
+    for (int i = 0; i < size; i++){
+        sum += arr[i];
+    }
+    average = sum / size;
+
+    printf("Sum: %d\n", sum);
+    printf("Average: %lf\n", average);
+
+    return EXIT_SUCCESS;
+}

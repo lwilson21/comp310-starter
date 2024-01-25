2c5dcad257678d3fa12b3988ff6031fc98d05997
Lee Wilson <awilson21@luc.edu>
2024-01-25 23:33:47 +0000

    Updated MatrixOps.c

--- bootcamp/WarmUp_5/MatrixOps.c
+++ bootcamp/WarmUp_5/MatrixOps.c
@@ -1,23 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//Can't return matricies from funtions, fix :(
 
-int main(int argc, char * * argv)
-{
-    int x[] = {1, 2};
-    int y[] = {3, 4};
-    
-    int aMat = addMatrix(x, y);
-    int mMat = multMatrix(x, y);
-
-    printf("Matrix addition: \n");
-    printMatrix(aMat);
-    printf("Matrix multiplication: \n");
-    printMatrix(mMat);    
-
-    return EXIT_SUCCESS;
-
-    int[][] addMatrix(int row[], int col[]){
+int[][] addMatrix(int row[], int col[]){
     int sizeRow = sizeof(row) / sizeof(row[0]);
     int sizeCol = sizeof(col) / sizeof(col[0]);
     
@@ -28,11 +14,12 @@ int main(int argc, char * * argv)
             matrix[i][j] = row[i] + col[j];
         }
     }
+    
 
     return matrix;
 }
 
-int[][] multMatrix(int row[], int col[]){
+void multMatrix(int row[], int col[]){
     int sizeRow = sizeof(row) / sizeof(row[0]);
     int sizeCol = sizeof(col) / sizeof(col[0]);
     
@@ -46,6 +33,7 @@ int[][] multMatrix(int row[], int col[]){
 
     return matrix;
 }
+
 void printMatrix(int matrix[][]){
     
     for (int i = 0; i < sizeX; i++){
@@ -55,4 +43,14 @@ void printMatrix(int matrix[][]){
         printf("\n");
     }
 }
+
+int main(int argc, char * * argv)
+{
+    int x[] = {1, 2};
+    int y[] = {3, 4};
+    
+    int aMat = addMatrix(x, y);
+    int mMat = multMatrix(x, y);
+
+    return EXIT_SUCCESS;
 }

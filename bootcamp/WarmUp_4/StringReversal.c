78b19bfa79879f865e836f3c192353838fcb5abb
Lee Wilson <awilson21@luc.edu>
2024-01-25 16:36:33 +0000

    Finished StringReversal.c

--- bootcamp/WarmUp_4/StringReversal.c
+++ bootcamp/WarmUp_4/StringReversal.c
@@ -1,10 +1,27 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char * * argv)
 {
-    String s = "Hello";
-    
+    char s[] = {'H', 'e', 'l', 'l', 'o'};
+    int size = sizeof(s)/sizeof(s[0]);
+    char r[size];
+
+    for (int i = 0; i < size; i++){
+        r[size - 1 - i] = s[i];
+    }
+
+    printf("Original string: ");
+    for (int i = 0; i < size; i++){
+        printf("%c", s[i]);
+    }
+    printf("\n");
+
+    printf("Reversed string: ");
+        for (int i = 0; i < size; i++){
+        printf("%c", r[i]);
+    }
+    printf("\n");
+
     return EXIT_SUCCESS;
 }

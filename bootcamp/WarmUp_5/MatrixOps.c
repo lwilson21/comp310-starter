4266604c34bd65d34c8a17c8d811e1fd7127a0cd
Lee Wilson <awilson21@luc.edu>
2024-01-27 16:35:56 +0000

    MatrixOps.c working. Revisit

--- bootcamp/WarmUp_5/MatrixOps.c
+++ bootcamp/WarmUp_5/MatrixOps.c
@@ -1,56 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Can't return matricies from funtions, fix :(
+//Since arrays can't be returned from functions in C, I decided that the addMatrix() and multMatrix() functions would 
+//return void and print the matrices as they are constructed.
 
-int[][] addMatrix(int row[], int col[]){
+void addMatrix(int row[], int col[]){
     int sizeRow = sizeof(row) / sizeof(row[0]);
     int sizeCol = sizeof(col) / sizeof(col[0]);
-    
-    int matrix[sizeRow][sizeCol];
+
+
+
+    printf("Added matrices: \n");
 
     for (int i = 0; i < sizeRow; i++){
         for (int j = 0; j < sizeCol; j++){
-            matrix[i][j] = row[i] + col[j];
+            printf("%d ", (row[i] + col[j]));
         }
+        printf("\n");
     }
-    
-
-    return matrix;
 }
 
 void multMatrix(int row[], int col[]){
     int sizeRow = sizeof(row) / sizeof(row[0]);
     int sizeCol = sizeof(col) / sizeof(col[0]);
-    
-    int matrix[sizeRow][sizeCol];
 
+    printf("Multiplied matrices: \n");
     for (int i = 0; i < sizeRow; i++){
         for (int j = 0; j < sizeCol; j++){
-            matrix[i][j] = row[i] * col[j];
-        }
-    }
-
-    return matrix;
-}
-
-void printMatrix(int matrix[][]){
-    
-    for (int i = 0; i < sizeX; i++){
-        for (int j = 0; j < sizeY; j++){
-            printf("%d ", matrix[i][j]);
+            printf("%d ", (row[i] * col[j]));
         }
         printf("\n");
     }
 }
+
+
 
 int main(int argc, char * * argv)
 {
     int x[] = {1, 2};
     int y[] = {3, 4};
     
-    int aMat = addMatrix(x, y);
-    int mMat = multMatrix(x, y);
+    addMatrix(x, y);
+    printf("\n\n");
+    multMatrix(x, y);
 
     return EXIT_SUCCESS;
 }

da8e414ca5a790f144a40022bcab89cb41183a12
Lee Wilson <awilson21@luc.edu>
2024-01-28 18:46:09 +0000

    Drafted LinkedList.c

--- bootcamp/WarmUp_8/LinkedList.c
+++ bootcamp/WarmUp_8/LinkedList.c
@@ -1,54 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct LinkedList{
-    Node head;
+struct Node{
+    int data;
+    struct Node *next;
+} head;
 
-    struct Node{
-        int data;
-        Node next;
+void push(int newData){
+    struct Node newNode = {
+        newData,
+        head
+    };
+    head = newNode;
+}
 
-        Node(int d) {
-            data = d;
-            next = NULL;
-        }
+void deleteNode(int key) {
+    struct Node temp = head;
+    struct Node prev = NULL;
+
+    if (temp != NULL && temp.data == key){
+        head = temp.next;
+        return;
     }
 
-    void push(int newData){
-        Node newNode = new Node(newData);
-        newNode.next = head;
-        head = newNode;
+    while (temp != NULL && temp.data != key) {
+        prev = temp;
+        temp = temp.next;
     }
 
-    void deleteNode(int key){
-        Node temp = head;
-        Node prev = NULL;
-
-        if (temp != NULL && temp.data == key){
-            head = temp.next;
-            return;
-        }
-
-        while (temp != NULL && temp.data != key){
-            prev = temp;
-            temp = temp.next;
-        }
-
-        if (temp == NULL){
-            return;
-        }
-
-        prev.next = temp.next;
+    if (temp == NULL){
+        return;
     }
 
-    void printList(){
+    prev.next = temp.next;
+}
 
+void printList() {
+    struct Node tnode = head;
+    while (tnode != NULL) {
+        printf("%d ", tnode.data);
+        tnode = tnode.next;
     }
 }
 
 
 int main(int argc, char * * argv)
 {
-    printf("Hello, World\n");
+    push(7);
+    push(1);
+    push(3);
+    push(2);
+
+    printList();
+
     return EXIT_SUCCESS;
 }

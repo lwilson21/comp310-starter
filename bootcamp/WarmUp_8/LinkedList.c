d492aaaf9b0e68d482c1582f1d2e0b0e4a70d0d9
Lee Wilson <awilson21@luc.edu>
2024-01-29 00:25:38 +0000

    Updated LinkedList.c

--- bootcamp/WarmUp_8/LinkedList.c
+++ bootcamp/WarmUp_8/LinkedList.c
@@ -1,60 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Node {
+/* I finished this warmup last and didn't really leave myself time to seek help, so that's my fault. 
+I don't fully understand structs. I also don't know if I used pointers right, or if they are 
+even necessary for the task. I just looked at the c-primer and tried my best, since I'm 90% sure we
+haven't talked about this material yet.*/
+
+struct Node{
     int data;
     struct Node *next;
-};
-
-struct Node *head;
+} *head;
 
 void push(int newData){
-    //make a new node with new data. head node should be next, head name should point to new node.
-
     struct Node newNode = {
         newData,
         head
     };
-    
+    head = &newNode;
 }
 
-void deleteNode(int key) {
-    struct Node temp = head;
-    struct Node prev;
-
-    if (temp != NULL && temp.data == key){
-        head = temp.next;
-        return;
-    }
-
-    while (temp != NULL && temp.data != key) {
-        prev = temp;
-        temp = temp.next;
-    }
-
-    if (temp == NULL){
-        return;    }
-
-    prev.next = temp.next;
-}
-
-void printList() {
-    struct Node tnode = head;
-    while (tnode != NULL) {
+void printList(){
+    struct Node tnode = *head;
+    while (){
         printf("%d ", tnode.data);
-        tnode = tnode.next;
+        tnode = *tnode.next;
     }
+    printf("\n"); 
 }
 
-
-int main(int argc, char * * argv)
-{
-    push(7);
+int main(int argc, char * * argv){
     push(1);
-    push(3);
     push(2);
 
     printList();
-
     return EXIT_SUCCESS;
 }
